302930902cbdbe5f56b2a82b451208ac51b1bd0a
Mikhail <ladonkin.mikhail@gmail.com>
2019-06-07 10:02:58 +0300

    Added the bars to the canvas

--- js/stat.js
+++ js/stat.js
@@ -2,7 +2,7 @@
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var GAP = 50;
-var CLOUD_X = 100;
+var CLOUD_X = 50;
 var CLOUD_Y = 50;
 var TEXT_WIDTH = 50;
 var FONT_GAP = 16;
@@ -17,14 +17,13 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
-
 window.renderStatistics = function (ctx, names, times) {
   var canvas = document.getElementById('winner-message');
   ctx = canvas.getContext('2d');
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 10, CLOUD_HEIGHT, CLOUD_WIDTH);
+  ctx.fillRect(110, 10, CLOUD_WIDTH, CLOUD_HEIGHT);
   ctx.fillStyle = 'white';
-  ctx.fillRect(100, 10, CLOUD_HEIGHT, CLOUD_WIDTH);
+  ctx.fillRect(100, 10, CLOUD_WIDTH, CLOUD_HEIGHT);
   ctx.fillStyle = 'black';
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
@@ -34,9 +33,9 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = '#000';
   var maxTime = getMaxElement(times);
   for (var i = 0; i < names.length; i++) {
-    var BarHeight = (200 * times[i] / maxTime);
-    ctx.fillText(names[i], CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_WIDTH) * i);
-    ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP + (GAP + BarHeight) * i, (BAR_WIDTH * times[i]) / maxTime, BAR_WIDTH);
+    var BarHeight = (150 * times[i] / maxTime);
+    ctx.fillText(names[i], CLOUD_X + GAP + FONT_GAP + (GAP + BAR_WIDTH) * i, CLOUD_Y + GAP);
+    ctx.fillRect(CLOUD_X + GAP + FONT_GAP + (GAP + BAR_WIDTH) * i, CLOUD_Y + GAP + TEXT_WIDTH, BAR_WIDTH, BarHeight);
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     }
