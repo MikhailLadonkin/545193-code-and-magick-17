f65454c93917ddaaaa374cf0115e8354f10a6e89
Mikhail <ladonkin.mikhail@gmail.com>
2019-06-12 15:23:57 +0300

    Fixed the color

--- js/stat.js
+++ js/stat.js
@@ -2,7 +2,7 @@
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var GAP = 50;
-var CLOUD_X = 50;
+var CLOUD_X = 140;
 var CLOUD_Y = 50;
 var TEXT_WIDTH = 50;
 var FONT_GAP = 16;
@@ -20,14 +20,14 @@ var getMaxElement = function (arr) {
 
 window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 10, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillRect(160, 20, CLOUD_WIDTH, CLOUD_HEIGHT);
   ctx.fillStyle = 'white';
-  ctx.fillRect(100, 10, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillRect(150, 10, CLOUD_WIDTH, CLOUD_HEIGHT);
   ctx.fillStyle = 'black';
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили!', 110, 20);
-  ctx.fillText('Список результатов:', 110, 35);
+  ctx.fillText('Ура вы победили!', 170, 20);
+  ctx.fillText('Список результатов:', 170, 40);
 
   var maxTime = getMaxElement(times);
   for (var i = 0; i < names.length; i++) {
@@ -38,8 +38,9 @@ window.renderStatistics = function (ctx, names, times) {
     } else {
       ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random().toFixed(2) + ')';
     }
-    ctx.fillText(names[i], X_COORDINATE, CLOUD_Y + GAP + 155);
-    ctx.fillText((Math.round(times[i] / 1000)), X_COORDINATE + 10, BarHeight + 220);
     ctx.fillRect(X_COORDINATE, 100 + CLOUD_Y + GAP + TEXT_WIDTH, BAR_WIDTH, BarHeight);
+    ctx.fillStyle = 'black';
+    ctx.fillText(names[i], X_COORDINATE, CLOUD_Y + GAP + 160);
+    ctx.fillText((Math.round(times[i])), X_COORDINATE, BarHeight + 220);
   }
 };
