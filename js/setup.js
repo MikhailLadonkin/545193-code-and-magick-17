a896d5293b47d66280c38e60cdf93c45adab982d
Mikhail <ladonkin.mikhail@gmail.com>
2019-06-13 13:23:27 +0300

    fixed the extra calls issue

--- js/setup.js
+++ js/setup.js
@@ -3,6 +3,41 @@ var setup = document.querySelector('.setup');
 var setupSimilar = document.querySelector('.setup-similar');
 var setupSimilarList = document.querySelector('.setup-similar-list');
 var template = document.querySelector('#similar-wizard-template').content.querySelector('div');
+var names = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
+var lastNames = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+];
+var coatColors = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+var eyesColors = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
 
 var deleteClass = function (variable, selector) {
   variable.classList.remove(selector);
@@ -10,41 +45,6 @@ var deleteClass = function (variable, selector) {
 
 var generateWizards = function (num) {
   var similarCharacters = [];
-  var names = [
-    'Иван',
-    'Хуан Себастьян',
-    'Мария',
-    'Кристоф',
-    'Виктор',
-    'Юлия',
-    'Люпита',
-    'Вашингтон'
-  ];
-  var lastNames = [
-    'да Марья',
-    'Верон',
-    'Мирабелла',
-    'Вальц',
-    'Онопко',
-    'Топольницкая',
-    'Нионго',
-    'Ирвинг'
-  ];
-  var coatColors = [
-    'rgb(101, 137, 164)',
-    'rgb(241, 43, 107)',
-    'rgb(146, 100, 161)',
-    'rgb(56, 159, 117)',
-    'rgb(215, 210, 55)',
-    'rgb(0, 0, 0)'
-  ];
-  var eyesColors = [
-    'black',
-    'red',
-    'blue',
-    'yellow',
-    'green'
-  ];
   for (var i = 0; i < num; i++) {
     similarCharacters.push(
         {
